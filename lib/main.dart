fce616a15c72df1fb264c7aee30866b6df6729f2
ivanbersenev <bersenev4dev@yandex.ru>
2024-07-11 00:28:38 +0700

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,20 @@
+import 'dart:async';
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:yndx_todo/app.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:yndx_todo/core/data/http_overrides.dart';
+import 'package:yndx_todo/core/logger.dart';
 
 void main() async {
-  // runZonedGuarded(
-  //   () async {
-  //     HttpOverrides.global = MyHttpOverrides();
-  //     await Hive.initFlutter();
-  //     runApp(const Providers());
-  //   },
-  //   (e, s) {
-  //     logger.e(e, stackTrace: s);
-  //   },
-  // );
-  HttpOverrides.global = MyHttpOverrides();
-  await Hive.initFlutter();
-  runApp(const App());
+  runZonedGuarded(
+    () async {
+      HttpOverrides.global = MyHttpOverrides();
+      await Hive.initFlutter();
+      runApp(const App());
+    },
+    (e, s) {
+      logger.e(e, stackTrace: s);
+    },
+  );
 }
